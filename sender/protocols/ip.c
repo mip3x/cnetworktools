15dab229669b4c7e3891d91858a8a6657dc9d78d
mip3x <plombiiiri@yandex.ru>
2025-03-08 19:53:44 +0300

    wip: ip header construction

--- sender/protocols/ip.c
+++ sender/protocols/ip.c
@@ -1,4 +1,6 @@
 #include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ether.h>
 #include <sys/ioctl.h>
 #include <string.h>
 #include <stdio.h>
@@ -13,6 +15,22 @@ enum status get_ip_addr(struct state* state) {
         puts("error in SIOCGIFADDR"); 
         return ERROR;
     }
+
+    return OK;
+}
+
+enum status construct_ip_header(struct state state) {
+    struct iphdr* iph = (struct iphdr*)(state.sendbuff + sizeof(struct ethhdr));
+    iph->version = 4;
+    iph->ihl = 5;
+    iph->tos = 16;
+    iph->id = htons(ID_VALUE);
+    iph->ttl = 64;
+    iph->protocol = 17;
+    iph->saddr = inet_addr(inet_ntoa( ((struct sockaddr_in*) &state.ifreq_ip.ifr_addr)->sin_addr));
+    iph->daddr = inet_addr(inet_ntoa( ((struct sockaddr_in*) &state.ifreq_ip.ifr_addr)->sin_addr));
+
+    state.packet_length += sizeof(struct iphdr);
 
     return OK;
 }
--- sender/protocols/ip.h
+++ sender/protocols/ip.h
@@ -4,6 +4,9 @@
 #include "../core/status.h"
 #include "../core/state.h"
 
+#define ID_VALUE 10201
+
 enum status get_ip_addr(struct state* state);
+enum status construct_ip_header(struct state state);
 
 #endif
